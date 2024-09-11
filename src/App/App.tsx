99cfe707262266b3d2a7a6021602373bdacd5a22
Lorena Martínez <lmartmol@gmail.com>
2024-09-11 18:12:46 +0200

    Add Header component

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import { Header } from "../components/Header.tsx";
 
 export const App = () => {
   return (
     <div className="main-container">
+      <Header />
       <Outlet />
     </div>
   );
--- src/pages/KatasPage/KatasPage.tsx
+++ src/pages/KatasPage/KatasPage.tsx
@@ -1,28 +1,10 @@
-import "./KatasPage.css";
 import { NavLink } from "react-router-dom";
 import { paths } from "../../config/paths.ts";
 
 export const KatasPage = () => {
   return (
     <>
-      <h1 className="main-title">
-        <span>Bienvenidos/as al taller</span>
-        <span>Frontend Testing: Consejos y Buenas Prácticas</span>
-      </h1>
-      <div className="images">
-        <img
-          src="/assets/pulpi-dereita-logo.png"
-          alt="Logo Pulpo conf logo"
-          width={128}
-          height={128}
-        />
-        <img
-          src="/assets/testing-library-logo.png"
-          alt="Logo Testing Library"
-          width={128}
-          height={128}
-        />
-      </div>
+      <h2>Listado de Katas: </h2>
       <ul className="links">
         <li>
           <NavLink to={paths.firstKata} className="link">
