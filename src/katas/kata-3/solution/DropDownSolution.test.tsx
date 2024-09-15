1a5b58543c4f3a2b78d70ce83c9bb9def794be71
Lorena Martínez <lmartmol@gmail.com>
2024-09-15 15:55:58 +0200

    Add kata 3 solution

---
+++ src/katas/kata-3/solution/DropDownSolution.test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import DropdownComponent from "../DropDown/DropDown.tsx";
+import { userEvent } from "@testing-library/user-event";
+
+describe("Given a DropdownComponent", () => {
+  describe("When the user selects an option", () => {
+    test("Then user should see the button with the selected option", async () => {
+      const user = userEvent.setup();
+      const firstOption = "Option 1";
+      const defaultButtonText = "Select an option";
+
+      render(<DropdownComponent />);
+
+      const button = screen.getByRole("button", { name: defaultButtonText });
+      await user.click(button);
+
+      const select = screen.getByRole("combobox");
+      await user.selectOptions(select, [firstOption]);
+
+      const selectedOption = screen.getByRole("button", {
+        name: `Selected: ${firstOption}`,
+      });
+
+      const selectAfterSelectOption = screen.queryByRole("combobox");
+
+      expect(selectAfterSelectOption).not.toBeInTheDocument();
+      expect(selectedOption).toBeVisible();
+    });
+  });
+});
